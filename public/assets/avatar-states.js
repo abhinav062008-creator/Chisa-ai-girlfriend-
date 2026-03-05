5999900b47596efde0ee3b13ef65b4837ae8d481
abhinav062008-creator <abhinav062008@gmail.com>
2026-03-05 20:01:45 +0530

    Create avatar-states.js

---
+++ public/assets/avatar-states.js
@@ -0,0 +1,9 @@
+const AVATAR_STATES = {
+    gentle: { imageUrl: "https://i.ibb.co/ns1GSDsT/Screenshot-2026-03-03-13-00-10-52-40deb401b9ffe8e1df2f1cc5ba480b12.jpg" },
+    flirty: { imageUrl: "https://i.ibb.co/ns1GSDsT/Screenshot-2026-03-03-13-00-10-52-40deb401b9ffe8e1df2f1cc5ba480b12.jpg" },
+    caring: { imageUrl: "https://i.ibb.co/ns1GSDsT/Screenshot-2026-03-03-13-00-10-52-40deb401b9ffe8e1df2f1cc5ba480b12.jpg" },
+    happy: { imageUrl: "https://i.ibb.co/ns1GSDsT/Screenshot-2026-03-03-13-00-10-52-40deb401b9ffe8e1df2f1cc5ba480b12.jpg" },
+    curious: { imageUrl: "https://i.ibb.co/ns1GSDsT/Screenshot-2026-03-03-13-00-10-52-40deb401b9ffe8e1df2f1cc5ba480b12.jpg" },
+    excited: { imageUrl: "https://i.ibb.co/ns1GSDsT/Screenshot-2026-03-03-13-00-10-52-40deb401b9ffe8e1df2f1cc5ba480b12.jpg" }
+};
+if (typeof window !== 'undefined') window.AVATAR_STATES = AVATAR_STATES;
